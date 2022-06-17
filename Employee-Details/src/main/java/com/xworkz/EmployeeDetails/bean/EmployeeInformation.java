25fbeb3747269609f129e69c006e6a8e22138efd
srivignesh123xworkz@gmail.com <srivignesh123xworkz@gmail.com>
2022-06-17 17:30:54 +0530

    Adding dynamic web application (Tomcat) (15,16-22)

--- Employee-Details/src/main/java/com/xworkz/EmployeeDetails/bean/EmployeeInformation.java
+++ Employee-Details/src/main/java/com/xworkz/EmployeeDetails/bean/EmployeeInformation.java
@@ -1,29 +1,48 @@
 package com.xworkz.EmployeeDetails.bean;
 
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-
 @RequestMapping("/")
 public class EmployeeInformation {
-	
-	
+
 	public EmployeeInformation() {
 
-	System.out.println("Default Employee Informatuion Running ");
-	
+		System.out.println("Default Employee Information Running ");
+
 	}
-	
+
+	/*
+	 * @RequestMapping("/EmployeeDetailsss.employee") public String
+	 * OnStartingEmployeeDetails(@RequestParam String name, @RequestParam int age,
+	 * 
+	 * @RequestParam boolean working) {
+	 * System.out.println("OnStartingEmployeeDetails is Running");
+	 * System.out.println(name); System.out.println(age);
+	 * System.out.println(working); return "EmployeeFile.html";
+	 * 
+	 * }
+	 */
+
 	@RequestMapping("/EmployeeDetails.employee")
-	public String OnStartingEmployeeDetails()
-	{
-		System.out.println("OnStartingEmployeeDetails is Running");
-		return "EmployeeFile.html";
+	public String OnEmployeeLogInForm(@RequestParam("empName") String empName,@RequestParam int age,@RequestParam String email, @RequestParam String password, @RequestParam String address, Model model) {
+		System.out.println("On Employee Log In Form is Running");
+
+		System.out.println(empName);
+		System.out.println(age);
+		System.out.println(email);
+		System.out.println(password);
+		System.out.println(address);
+		model.addAttribute("name", empName);
+		model.addAttribute("age", age);
+		model.addAttribute("email", email);
+		model.addAttribute("password", password);
+		model.addAttribute("address", address);
+
+		return "Employee.jsp";
 	}
-	
-	
-	
-	
 
 }
---
+++ Employee-Details/src/main/webapp/Employee.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Successfully Registered</title>
+</head>
+<body>
+
+	<h1>Displaying Employers Registration Details</h1>
+
+	<h2>
+
+		name:=<b>${name}</b><br> age:=<b>${age}</b><br> email:=<b> ${email}</b><br>
+
+		password:=<b>${password}</b><br> address:=<b>${address}</b><br>
+
+	</h2>
+
+
+</body>
+</html>
